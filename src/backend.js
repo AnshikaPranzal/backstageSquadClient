c0abc01de07b95421fc7973998ee1343ad48ba01
Anshika-Pranzal <anshika.pranzal@jupiter.money>
2021-11-29 01:00:54 +0530

    api options

--- src/backend.js
+++ src/backend.js
@@ -1,1 +1,2 @@
-export const API = 'http://localhost:8001/api';
+// export const API = 'http://localhost:8001/api'; //switch to this to run local server
+export const API = 'https://backstage-squad.herokuapp.com/api'
